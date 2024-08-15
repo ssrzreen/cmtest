30e5d14a4f3ed42e90ca180adddeb9d20dc76bef
ssrzreen <ssrzreenchannel@gmail.com>
2024-08-15 15:13:58 +0700

    CM test Application

--- app/src/main/java/com/example/cmapplication/volleySingletion/VolleySingleton.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cmapplication.volleySingletion
-
-import android.content.Context
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.Volley
-
-class VolleySingleton(context: Context) {
-
-}
